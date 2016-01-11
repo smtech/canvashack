b265b1d06c1c65a171547fc719462e7c8df8ca79
Seth Battis <sethbattis@stmarksschool.org>
2016-01-11 16:41:34 -0500

    $_SESSION['canvasInstanceUrl']
    …is a lot more useful when it's set.

--- common-app.inc.php
+++ common-app.inc.php
@@ -1,5 +1,8 @@
 <?php
-	
+
+$_SESSION['canvasInstanceUrl'] = 'https://' . $_SESSION['toolProvider']->user->getResourceLink()->settings['custom_canvas_api_domain'];
+$api = new CanvasPest($_SESSION['apiUrl'], $_SESSION['apiToken']);
+
 /* common functions for your app could go here -- it's automatically included in common.inc.php */
 $smarty->assign('name', 'CanvasHack');
 $smarty->assign('category', 'Configuration');
