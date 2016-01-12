9fb83b42fb6ba12c16a0f340e761316b83209153
Seth Battis <sethbattis@stmarksschool.org>
2016-01-12 10:41:35 -0500

    Not always called by LTI
    Be more careful (e.g. when canvashack.css and canvashack.js are loaded by _everything_).

--- common-app.inc.php
+++ common-app.inc.php
@@ -1,7 +1,12 @@
 <?php
 
-$_SESSION['canvasInstanceUrl'] = 'https://' . $_SESSION['toolProvider']->user->getResourceLink()->settings['custom_canvas_api_domain'];
-$api = new CanvasPest($_SESSION['apiUrl'], $_SESSION['apiToken']);
+if (defined('IGNORE_LTI')) {
+	$_SESSION['canvasInstanceUrl'] = 'https://' . parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
+	$api = new CanvasPest($metadata['CANVAS_API_URL'], $metadata['CANVAS_API_TOKEN']);
+} else {
+	$_SESSION['canvasInstanceUrl'] = 'https://' . $_SESSION['toolProvider']->user->getResourceLink()->settings['custom_canvas_api_domain'];
+	$api = new CanvasPest($_SESSION['apiUrl'], $_SESSION['apiToken']);
+}
 
 /* common functions for your app could go here -- it's automatically included in common.inc.php */
 $smarty->assign('name', 'CanvasHack');
