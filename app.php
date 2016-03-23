16c609d73a5b77f01ef2052fc4dcdc0301fea8c1
Seth Battis <sethbattis@stmarksschool.org>
2016-03-23 12:53:30 -0400

    Update package versions

--- app.php
+++ app.php
@@ -2,20 +2,16 @@
 
 require_once('common.inc.php');
 
+use smtech\CanvasPest\CanvasPest;
+
 /* replace the contents of this file with your own app logic */
 
 $api = new CanvasPest($_SESSION['apiUrl'], $_SESSION['apiToken']);
 $profile = $api->get('/users/self/profile');
 
 $smarty->assign('content', "
-	<h1>App</h1>
-	
-	<h2>{$_REQUEST['lti-request']} Request</h3>" .
-	(isset($_REQUEST['reason']) ?
-		"<p>{$_REQUEST['reason']}</p>" : ''
-	) . "
-	<h2>GET /users/self/profile</h3>		
-	<pre>" . print_r($profile, true) . '</pre>'
+	<h1>Tool Provider Settings</h1>
+	<pre>" . print_r($toolProvider->user->getResourceLink()->settings, true) . '</pre>'
 );
 
 $smarty->display();
