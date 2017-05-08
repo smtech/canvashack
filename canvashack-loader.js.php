a27cb0e6e618d6adceb837ec7318032133876f1c
Seth Battis <sethbattis@stmarksschool.org>
2017-05-08 16:16:23 -0400

    Load jQuery. Anyway.
    Address [this issue](https://github.com/smtech/canvashack-plugin-stmarks-branding/issues/4) until [this ticket](https://cases.canvaslms.com/CommunityConsole?id=500A000000WMHl6IAH) is resolved.

--- canvashack-loader.js.php
+++ canvashack-loader.js.php
@@ -9,10 +9,33 @@ if (!empty($_REQUEST['download'])) {
 }
 
 ?>
-var args = {
-  current_user: ENV.current_user,
-  current_user_roles: ENV.current_user_roles,
-  location: window.location.href
-};
-$('head').append('<link id="canvashack-dynamic-css" rel="stylesheet" href="<?= $toolbox->config('APP_URL') ?>/canvashack.css.php?' + $.param(args) + '" />');
-$.getScript('<?= $toolbox->config('APP_URL') ?>/canvashack.js.php?' + $.param(args));
+function loadJQuery() {
+    var cdn = document.createElement('script');
+    cdn.setAttribute('src', 'https://code.jquery.com/jquery-1.7.2.min.js');
+    cdn.setAttribute('crossorigin', 'anonymous');
+    document.getElementsByTagName('body')[0].appendChild(cdn);
+}
+
+function loadCanvasHack() {
+    var args = {
+      current_user: ENV.current_user,
+      current_user_roles: ENV.current_user_roles,
+      location: window.location.href
+    };
+
+    $('head').append('<link id="canvashack-dynamic-css" rel="stylesheet" href="<?= $toolbox->config('APP_URL') ?>/canvashack.css.php?' + $.param(args) + '" />');
+    $.getScript('<?= $toolbox->config('APP_URL') ?>/canvashack.js.php?' + $.param(args));
+}
+
+function checkJQuery() {
+    if (window.jQuery !== undefined) {
+        loadCanvasHack();
+    } else {
+        window.setTimeout(checkJQuery, 25);
+    }
+}
+
+if (window.jQuery === undefined) {
+    loadJQuery();
+}
+checkJQuery();
