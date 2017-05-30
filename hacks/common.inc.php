711487fa4828cd083ad4a0a28cccffe3e232cb74
Seth Battis <sethbattis@stmarksschool.org>
2017-05-30 14:50:43 -0400

    More careful argument processing
    Also, updated dependencies

--- hacks/common.inc.php
+++ hacks/common.inc.php
@@ -4,6 +4,6 @@ define('IGNORE_LTI', true);
 
 require_once __DIR__ . '/../common.inc.php';
 
-if (empty($_REQUEST)) {
+if (empty($_REQUEST) && !empty($argv[1])) {
     $_REQUEST = unserialize($argv[1]);
 }
